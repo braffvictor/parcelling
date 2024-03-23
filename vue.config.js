9e2a73b63be80f9e26cecda2be86653ba736e1a8
swaedavid <swaedavid@gmail.com>
2024-03-23 01:58:34 +0100

    updated

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,10 @@ module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     themeColor: "#001b3a",
+    msTileColor: "#001b3a",
+    appleMobileWebAppCache: "yes",
+    manifestOptions: {
+      background_color: "#001b3a",
+    },
   },
 });
