eccd025a7b10c30c22ffe203b21eebc0c0795726
swaedavid <swaedavid@gmail.com>
2024-12-07 11:50:29 +0100

    updated

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     iconPaths: {
-      faviconSVG: "@/src/assets/favicon.ico",
-      favicon32: "@/src/assets/favicon-32x32.png",
-      favicon16: "@/src/assets/favicon-16x26.png",
-      appleTouchIcon: "@/src/assets/apple-touch-icon-152x152.png",
-      maskIcon: "@/src/assets/android-chrome-192x192.png",
-      msTileImage: "@/src/assets/android-chrome-512x512.png",
+      faviconSVG: "/favicon.ico",
+      favicon32: "/favicon-32x32",
+      favicon16: "/favicon-16x26.png",
+      appleTouchIcon: "/apple-touch-icon-152x152.png",
+      maskIcon: "/android-chrome-192x192.png",
+      msTileImage: "/android-chrome-512x512.png",
     },
     themeColor: "#001b3a",
     msTileColor: "#001b3a",
